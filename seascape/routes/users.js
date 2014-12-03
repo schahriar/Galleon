ba19bbd1aaf2bd2a6dfa6a64610d43f07c4dde63
schahriar <info@schahriar.com>
2014-12-03 12:26:24 -0800

    Get users test

--- seascape/routes/users.js
+++ seascape/routes/users.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
+	app.models.users.find().exec(function(error, models) {
+		if(error) return res.json({ error: error }, 500);
+		res.json(models);
+	});
 	res.send('Users');
 });
 
