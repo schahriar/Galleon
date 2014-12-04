31d27ff920603eb2b17a6be5900d4045cc46a586
schahriar <info@schahriar.com>
2014-12-03 21:49:52 -0800

    Improved code

--- seascape/routes/mailbox.js
+++ seascape/routes/mailbox.js
@@ -6,12 +6,9 @@ router.get('/', function(req, res) {
 	if(!req.authenticated) return res.status(500).json({ error: "Not Authenticated" });
 	
 	// Add better error handling here
-	var models = req.database.models;
-	models.users.findOne({req.authenticated.email}).exec(function(error, user){
-		models.mail.find().where({ email: user.email }).exec(function(error, mails){
-			if(error) res.status(500).json({ error: "Not Authenticated" });
-			res.render('mailbox', { mails: mails });
-		});
+	req.database.models.mail.find().where({ email: req.authenticated.email }).exec(function(error, mails){
+		if(error) res.status(500).json({ error: "Not Authenticated" });
+		res.render('mailbox', { mails: mails });
 	});
 });
 
