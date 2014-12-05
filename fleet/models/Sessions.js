fbf9c966e1960fea510d4cda429f6b3859e29599
schahriar <info@schahriar.com>
2014-12-05 00:21:57 -0800

    Added Cross-database session id

--- fleet/models/Sessions.js
+++ fleet/models/Sessions.js
@@ -1,3 +1,7 @@
+var crypto = require('crypto');
+var md5sum = crypto.createHash('md5');
+var shortId = require('shortid');
+
 module.exports = {
 	// Idenitity is a unique name for this model
   	identity: 'sessions',
@@ -10,6 +14,13 @@ module.exports = {
 	},
 	
 	attributes: {
+		sessionID: {
+			type: 'string',
+			required: true,
+			maxLength: 48,
+			unique: true,
+		},
+		
 		email: {
 			type: 'string',
 			required: true,
@@ -30,5 +41,12 @@ module.exports = {
 		  type: 'json',
 		  stamp: true
 		}
+	},
+	
+	beforeCreate: function(attributes, callback) {
+		// Should round up about 14 + 2 + 32 = 48 characters at max
+		// Hashsum enables email checking without exposing the email
+		// to session token.
+		attributes.sessionID = shortId.generate() + '__' + md5sum.update(attributes.email).digest('hex');;
 	}
 };
