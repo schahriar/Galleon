065c083f05ad5ade11f1acbcc8493d36b56e030c
schahriar <info@schahriar.com>
2014-12-04 23:46:36 -0800

    Removed duplicated check

--- seascape/routes/mailbox.js
+++ seascape/routes/mailbox.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-	if(!req.authenticated) return res.status(500).json({ error: "Not Authenticated" });
-	
+router.get('/', function(req, res) {	
 	// Add better error handling here
 	req.database.models.mail.find().where({ email: req.authenticated.email }).exec(function(error, mails){
 		if(error) res.status(500).json({ error: "Not Authenticated" });
