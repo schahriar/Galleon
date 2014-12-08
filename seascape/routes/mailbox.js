b609d40b0fac2ddaec93321130318b790d359a0a
schahriar <info@schahriar.com>
2014-12-08 11:57:22 -0800

    Fixed authentication bug

--- seascape/routes/mailbox.js
+++ seascape/routes/mailbox.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-	if(!req.authenticated) return res.status(500).json({ error: "Not Authenticated" });
+	if(req.authenticated == false) return res.status(500).json({ error: "Not Authenticated" });
 	
 	// Add better error handling here
 	req.database.models.mail.find().where({ email: req.authenticated.email }).exec(function(error, mails){
 		if(error) res.status(500).json({ error: "Not Authenticated" });
+		if((!mails)||(mails.length < 1)) mails = [];
 		res.render('mailbox', { mails: mails });
 	});
 });
