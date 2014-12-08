26f3ee3422755dbe0205384ee43360950add984b
schahriar <info@schahriar.com>
2014-12-08 12:33:07 -0800

    Fixed a Database parameter

--- seascape/routes/mailbox.js
+++ seascape/routes/mailbox.js
@@ -5,7 +5,7 @@ router.get('/', function(req, res) {
 	req.getCredentials(function(error, credentials){
 		if(error) res.status(500).json({ error: "Not Authenticated" });
 		// Add better error handling here
-		req.database.models.mail.find().where({ email: credentials.email }).exec(function(error, mails){
+		req.database.models.mail.find().where({ receiver: credentials.email }).exec(function(error, mails){
 			if(error) res.status(500).json({ error: "Not Authenticated" });
 			if((!mails)||(mails.length < 1)) mails = [];
 			res.render('mailbox', { mails: mails });
