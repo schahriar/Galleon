798ba7e9600ae8d617de220a3be8d9bd8e7cd45d
schahriar <info@schahriar.com>
2014-12-09 11:43:01 -0800

    Removed ~less-middleware~

--- seascape/server.js
+++ seascape/server.js
@@ -27,7 +27,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('Your_very_secret_cookie_code'));
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Database middleware
 app.use(function (req, res, next) {
@@ -43,9 +44,6 @@ app.use('/login', login);
 app.use('/users', users);
 app.use('/mail', mail);
 app.use('/mailbox', mailbox);
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
