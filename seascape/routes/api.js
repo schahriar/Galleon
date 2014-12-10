df2cee9a9e59d22097fc60c72336ff58be87b964
schahriar <info@schahriar.com>
2014-12-10 12:33:19 -0800

    Fixed a reference issue
    Copy pasted the code

--- seascape/routes/api.js
+++ seascape/routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var _ = require('lodash');
 var router = express.Router();
 
-router.get('/inbox', function(req, res) {
+router.get('/', function(req, res) {
 	req.getCredentials(function(error, credentials){
 		if(error) res.status(500).json({ error: "Not Authenticated" });
 		
