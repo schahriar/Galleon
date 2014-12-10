dcd4d19cff6455c10b9a022eec0860479f83a636
schahriar <info@schahriar.com>
2014-12-10 12:51:45 -0800

    Added a test module for BackBone

--- seascape/routes/api.js
+++ seascape/routes/api.js
@@ -11,7 +11,17 @@ router.route('/')
 		});
 	})
 	.get(function(req, res, next) {
-		res.json(req.body);
+		req.database.models.mail.find().where({ receiver: credentials.email, spamScore: { '<=': 5 } /* Spam filter */ }).exec(function(error, mails){
+			if(error) res.status(500).json({ error: "Not Authenticated" });
+			if((!mails)||(mails.length < 1)) mails = [];
+			
+			var filteredMails = [];
+			_(mails).forEach(function(mail) {
+				filteredMails.push(_.pick(mail,['eID','sender','receiver','to','stamp','subject','text','html','read'])); 
+			});
+			
+			res.json(filteredMails);
+		});
 	})
 	.put(function(req, res, next) {
 		res.json(req.body);
