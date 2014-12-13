becd2d99c9537a71a1f060881da191115416e8e7
schahriar <info@schahriar.com>
2014-12-12 18:47:15 -0800

    Added ripple effect

--- seascape/public/scripts/master.js
+++ seascape/public/scripts/master.js
@@ -160,6 +160,9 @@ client.build = function(){
 		},
 		
 		load: function(e) {
+			// Ripple effect
+			this.ripple(e);
+			
 			// Refresh elements
 			this.el = $("section#list");
 			this.view = $("#view");
@@ -188,6 +191,34 @@ client.build = function(){
 			newElement.find('.html').html(mail.get('html'));
 
 			this.view.html(newElement);
+		},
+		
+		/* Credits go to CodePlayer at http://thecodeplayer.com/walkthrough/ripple-click-effect-google-material-design */
+		ripple: function(e){
+			var parent = $(e.currentTarget).parent();
+			//create .ink element if it doesn't exist
+			if(parent.find(".ink").length == 0)
+				parent.prepend("<span class='ink'></span>");
+
+			var ink = parent.find(".ink");
+			//incase of quick double clicks stop the previous animation
+			ink.removeClass("animate");
+
+			//set size of .ink
+			if(!ink.height() && !ink.width())
+			{
+				//use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
+				d = Math.max(parent.outerWidth(), parent.outerHeight());
+				ink.css({height: d, width: d});
+			}
+
+			//get click coordinates
+			//logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
+			x = e.pageX - parent.offset().left - ink.width()/2;
+			y = e.pageY - parent.offset().top - ink.height()/2;
+
+			//set the position and add class .animate
+			ink.css({top: y+'px', left: x+'px'}).addClass("animate");
 		},
 
 		render: function() {
