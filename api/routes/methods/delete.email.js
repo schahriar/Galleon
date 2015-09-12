b563e9c2f9022cc9d12b14209a7241de35ecd3a5
Schahriar SaffarShargh <info@schahriar.com>
2015-09-12 14:55:53 -0700

    Fixed a security bug

--- api/routes/methods/delete.email.js
+++ api/routes/methods/delete.email.js
@@ -1,6 +1,5 @@
 module.exports = function(req, res) {
-	var credentials = req.credentials;
-	req.galleon.query('delete', { eID: req.param('eID') }, function(error) {
+	req.galleon.query('delete', { eID: req.param('eID'), email: req.credentials.email }, function(error) {
 		if(error) return res.status(500).json({ error: error });
 		res.json({ success: true })
 	});
