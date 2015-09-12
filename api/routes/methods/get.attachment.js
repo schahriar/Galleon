92ef44dbfcc93105ea82df235a893e0a08b51dfd
Schahriar SaffarShargh <info@schahriar.com>
2015-09-12 14:56:38 -0700

    Updated get attachment method
    There were issues with Express 4.0 sendFile which have been fixed here.

--- api/routes/methods/get.attachment.js
+++ api/routes/methods/get.attachment.js
@@ -1,14 +1,20 @@
+var path = require('path');
+
 module.exports = function(req, res) {
-    req.galleon.query('attachment', { eID: req.params.eID, email: req.credentials.email, id: req.params.id.toString() }, function(error, attachment){
+    req.galleon.query('getAttachment', { eID: req.params.eID, email: req.credentials.email, id: req.params.id.toString() }, function(error, attachment){
+        if (error) return res.status(400).json({ error: error.toString() })
         if (attachment.cid) {
+            console.log("CID ATTACHMENT", attachment)
             res.type(attachment.name);
-            res.sendFile(attachment.path, {
+            res.sendFile(path.basename(attachment.path), {
+                root: path.dirname(attachment.path),
                 dotfiles: 'deny',
                 headers: {
                     'x-timestamp': Date.now(),
                     'x-sent': true
                 }
             }, function(error) {
+                console.log(error)
                 if (error) res.status(error.status).end();
             })
         }
