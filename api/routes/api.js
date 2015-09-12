c43b56e1d1c8ef0f2bf14c06316f5203f359eb14
Schahriar SaffarShargh <info@schahriar.com>
2015-09-12 14:52:47 -0700

    Added support for outgoing attachments
    Added support for multi-part outgoing attachments with Multer.
    Limits are set to:
    15mb per file
    max 20 files
    max 20k parts

--- api/routes/api.js
+++ api/routes/api.js
@@ -2,6 +2,28 @@ var argv = require('yargs').argv;
 
 var express = require('express');
 var router = express.Router();
+// Multipart-Upload
+var multer = require('multer');
+// Attachment ID
+var shortId = require('shortid');
+
+// Multipart parser for attachment uploads
+var storage = multer.diskStorage({
+	destination: function (req, file, callback) {
+		callback(null, req.environment.paths.attachments || "\tmp")
+	},
+	filename: function (req, file, callback) {
+		/* SECURITY -> Email ID Must be validated */
+		callback(null, "+" + req.param("eID") + "_" + shortId.generate())
+	}
+})
+
+var upload = multer({ storage: storage, limits: {
+	fields: 20,
+	fileSize: 15000000,
+	files: 20,
+	parts: 20000,
+}})
 
 router.use(function(req, res, next) {
 	// runs for all HTTP verbs first
@@ -17,5 +39,7 @@ router.patch('/:eID', require("./methods/patch.email.js"));
 router.delete('/:eID', require("./methods/delete.email.js"));
 router.get('/:eID/attachment/:id', require("./methods/get.attachment.js"));
 router.post('/send', require("./methods/post.email.js"));
+router.post('/send/:eID/attachment', upload.single('attachment'), require("./methods/post.attachment.js"));
+router.delete('/send/:eID/attachment/:ref', require("./methods/delete.attachment.js"));
 
 module.exports = router;
---
+++ query/link.attachment.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var fs = require('fs');
+var _ = require('lodash');
+var crypto = require('crypto');
+
+module.exports = function(Galleon, query, callback) {
+    Galleon.connection.collections.queue.findOne({
+        association: { contains: query.email },
+        eID: query.eID.substring(1)
+    }).exec(function(error, mail) {
+        if (error) return callback(error);
+        if (!mail) return callback("Mail not found!");
+        if (!query.file) return callback("No file received");
+
+        // Make sure mail.attachments is an array
+        if (!_.isArray(mail.attachments)) mail.attachments = [];
+        
+        var reference = crypto.randomBytes(8).toString('hex');
+        
+        mail.attachments.push({
+            id: 	  (query.file.filename)?(query.file.filename.split("_")[1] || null):null,
+            cid:	  null, /* Not implemented yet */
+            fileName: query.file.originalname,
+            path: 	  query.file.path,
+
+            transferEncoding: query.file.encoding,
+            contentType: query.file.mimetype,
+            checksum: null, /* Not implemented yet */
+            length:   query.file.size,
+            
+            ref: reference
+        })
+        Galleon.connection.collections.queue.update({
+            association: { contains: query.email },
+            eID: query.eID.substring(1)
+        }, {
+            attachments: mail.attachments
+        }).exec(function(error, mail){
+            if (error) return callback(error);
+            if (!mail) return callback("Mail not found!");
+            
+            // Else
+            callback(null, { ref: reference });
+        })
+    })
+}
