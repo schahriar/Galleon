58e4b480c73aa34b41c0ecec43c1caf43979bb75
Schahriar SaffarShargh <info@schahriar.com>
2015-09-12 14:57:11 -0700

    Added error catching for modulator

--- bin/modulator/launch.js
+++ bin/modulator/launch.js
@@ -1,7 +1,9 @@
 var _ = require('lodash');
 var async = require('async');
 
-module.exports = function(modules, context, callback) {
+module.exports = function() {
+    var args = _.toArray(arguments);
+    var modules = args.shift(), callback = args.pop();
     var functions = [];
     // Populate functions
     _.each(modules, function(MODULE) {
@@ -9,5 +11,15 @@ module.exports = function(modules, context, callback) {
             require(MODULE.reference).exec.apply(MODULE, arguments);
         })
     })
-    async.applyEachSeries(functions, context, callback)
+    
+    // Ignore if no modules are registered for the current task
+    if(functions.length <= 0) return callback();
+    
+    /* Slows down module execution but prevents unintended crashes */
+    // Prevents a bad module from corrupting the entire eco-system
+    try {
+        async.applyEachSeries(functions, args, callback);
+    }catch(error){
+        callback(error);
+    }
 }
