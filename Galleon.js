0d9bfb7b029be55d0baaacd61e5118f5eb366d96
Schahriar SaffarShargh <info@schahriar.com>
2015-10-15 14:25:05 -0700

    Fixed a bug

--- Galleon.js
+++ Galleon.js
@@ -69,7 +69,7 @@ var Galleon = function(config, callback){
 		Defaults = config;
 	//
 
-	if((!config.environment) || (!config.env)) {
+	if((!config.environment) && (!config.env)) {
 		try {
 			config.environment = JSON.parse(fs.readFileSync(path.resolve(osenv.home(), '.galleon/', 'galleon.conf'), 'utf8'));
 		}catch(e) {
