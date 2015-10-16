d3f55e815ef11f659c2f1c92d8c6a26a059ecc82
Schahriar SaffarShargh <info@schahriar.com>
2015-10-16 01:44:02 -0700

    Updated Tests

--- test.js
+++ test.js
@@ -14,6 +14,8 @@ var CONNECTION = {
 	adapter: 'sails-memory',
 }
 
+var expect = chai.expect;
+
 describe("Test Suite", function() {
 	this.timeout(10000);
 	it("should create a new instance of Galleon", function(done) {
@@ -39,24 +41,42 @@ describe("Test Suite", function() {
 			});
 		})
 	})
-	it("should receive connections to SMTP server", function(done) {
-		connection.connect(function() {
+	// Add bcrypt test
+	it("should create a user", function(done) {
+		G.createUser({
+			email: "info@example.com",
+			password: "bestpasswordever",
+			name: "test"
+		}, function(error, user) {
+			if(error) throw error;
+			expect(user.email).to.equal("info@example.com");
+			expect(user.name).to.equal("test");
 			done();
 		})
 	})
-	it("should receive and process an email to the database", function(done) {
-		connection.send({
-			from: "test@example.com",
-			to: "info@example.com"
-		}, "From: me@domain.com\n\rTo: you@sample.com\n\rSubject: Example Message\n\rSending a test message.", function(error) {
-			if(error) throw error;
-			G.query('get', {
-				email: "info@example.com",
-				folder: 'inbox'
-			}, function(error, results) {
-				if(error) throw error;
-				console.log(results);
+	describe("SMTP incoming tests", function(){
+		it("should receive connections to SMTP server", function(done) {
+			connection.connect(function() {
 				done();
+			})
+		})
+		it("should receive and process an email to the database", function(done) {
+			connection.send({
+				from: "test@example.com",
+				to: "info@example.com"
+			}, "From: me@domain.com\nTo: you@sample.com\nSubject: Example Message\n\rSending a test message.", function(error) {
+				if(error) throw error;
+				setTimeout(function(){
+					G.query('get', {
+						email: "info@example.com",
+						folder: 'inbox',
+						page: 0
+					}, function(error, results) {
+						if(error) throw error;
+						expect(results[0].text).to.equal('Sending a test message.');
+						done();
+					})
+				}, 200)
 			})
 		})
 	})
