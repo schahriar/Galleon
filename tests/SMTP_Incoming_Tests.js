dc35e29de37c7251d4c0e58b2ccbef0dbb2f08ea
Schahriar SaffarShargh <info@schahriar.com>
2015-10-17 14:11:35 -0700

    Added Processing Tests

--- tests/SMTP_Incoming_Tests.js
+++ tests/SMTP_Incoming_Tests.js
@@ -7,23 +7,26 @@ describe("SMTP Incoming Tests", function () {
 			done();
 		})
 	})
-	it("should receive and process an email to the database", function (done) {
+	it("should receive an email to the database", function (done) {
 		global.connection.send({
 			from: "test@example.com",
 			to: "info@example.com"
 		}, "From: me@domain.com\nTo: you@sample.com\nSubject: Example Message\n\rSending a test message.", function (error) {
 			if (error) throw error;
-			setTimeout(function () {
-				global.galleon.query('get', {
-					email: "info@example.com",
-					folder: 'inbox',
-					page: 0
-				}, function (error, results) {
-					if (error) throw error;
-					expect(results[0].text).to.equal('Sending a test message.');
-					done();
-				})
-			}, 200)
+			done();
+		})
+	})
+	it("should process incoming emails ot the database", function (done) {
+		process.nextTick(function () {
+			global.galleon.query('get', {
+				email: "info@example.com",
+				folder: 'inbox',
+				page: 0
+			}, function (error, results) {
+				if (error) throw error;
+				expect(results[0].text).to.equal('Sending a test message.');
+				done();
+			})
 		})
 	})
 })
