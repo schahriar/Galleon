4c67b0a82ac7be7637e7c4d8f82fcf0cc7c90148
Schahriar SaffarShargh <info@schahriar.com>
2015-10-31 15:36:58 -0700

    [fix] Modulator Update Query fixed

--- bin/modulator/update.js
+++ bin/modulator/update.js
@@ -6,7 +6,7 @@ module.exports = function(name, config) {
     // Modules are likely required to be loaded on start thus will be loaded Synchronously
     _.each(environment.getModulesSync(), function(MODULE) {
         // If Current Module Matches name
-        if(MODULE.name === name) {
+        if((MODULE.name.toLowerCase() === name) || (MODULE.reference.toLowerCase() === name)) {
             // Update Config
             environment.updateModuleConfig(MODULE, config);
             MatchFound = true;
