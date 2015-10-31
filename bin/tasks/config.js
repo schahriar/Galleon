6c1a1864b6944564876dff7639c992575e0bed8d
Schahriar SaffarShargh <info@schahriar.com>
2015-10-31 13:01:44 -0700

    Added Config CLI Command

---
+++ bin/tasks/config.js
@@ -0,0 +1,16 @@
+var _ = require('lodash');
+var Modulator = require('../modulator/master');
+
+module.exports = function(Galleon, argv) {
+    // Format -> galleon config <module_name> <config_name> <config_value>
+    
+    // Remove config from argv
+    argv._.shift();
+    var Config = {};
+    try {
+        Config[argv._[1]] = argv._[2];
+    }catch(e) {
+        throw new Error("Failed to update Config. Try the following format \n galleon config <module_name> <config_name> <config_value>");
+    }
+    Modulator.update(argv._[0], Config);
+}
